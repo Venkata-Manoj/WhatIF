5a7deea2b0d2db5c13118e849046c4cc6dc01e20
Venkata-Manoj <177463638+Venkata-Manoj@users.noreply.github.com>
2025-09-01 21:26:46 +0530

    Update types.ts

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,12 +1,18 @@
 import type { AnalyzeComponentOutput } from '@/ai/flows/analyze-component';
+import type { IdentifyComponentRisksOutput } from '@/ai/flows/identify-component-risks';
+import type { GeneratePreventativeSuggestionsOutput } from '@/ai/flows/generate-preventative-suggestions';
 import type { Timestamp } from 'firebase/firestore';
+
+export type Risk = IdentifyComponentRisksOutput['risks'][0];
+export type Suggestion = GeneratePreventativeSuggestionsOutput['suggestions'][0];
+
 
 export type AnalysisResult = {
   id?: string;
   componentName: string;
   analysis: AnalyzeComponentOutput;
-  risks: string;
-  suggestions: string;
+  risks: Risk[];
+  suggestions: Suggestion[];
   checklist: string;
   createdAt?: Timestamp | { seconds: number; nanoseconds: number; };
 };
