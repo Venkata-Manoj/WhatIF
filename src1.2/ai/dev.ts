1feb8f018a7034c0d369fc765a126b993c578117
Venkata-Manoj <177463638+Venkata-Manoj@users.noreply.github.com>
2025-09-06 14:53:31 +0530

    Delete src1.2 directory

--- src1.2/ai/dev.ts
+++
@@ -1,8 +0,0 @@
-
-import { config } from 'dotenv';
-config();
-
-import '@/ai/flows/analyze-component.ts';
-import '@/ai/flows/generate-preventative-suggestions.ts';
-import '@/ai/flows/generate-developer-checklist.ts';
-import '@/ai/flows/identify-component-risks.ts';
